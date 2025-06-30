03fc4840df4e31ef744df83043726efffd67516d
irexyc <irexyc@gmail.com>
2025-06-30 18:26:56 +0800

    support sleep/wakeup for pt engine

--- lmdeploy/pytorch/engine/model_agent.py
+++ lmdeploy/pytorch/engine/model_agent.py
@@ -4,7 +4,7 @@ import base64
 import functools
 from contextlib import asynccontextmanager, contextmanager
 from multiprocessing.reduction import ForkingPickler
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional
 
 import torch
 import torch.distributed as dist
@@ -892,6 +892,29 @@ class BaseModelAgent:
                     mod.update_weights()
 
             torch.cuda.empty_cache()
+
+    @torch.inference_mode()
+    def sleep(self, tags: Optional[List[str]] = None):
+        """Sleep."""
+        if tags is None:
+            tags = ['weights', 'kv_cache']
+        if 'weights' in tags:
+            # TODO: find better way to avoid reset graph
+            self.reset_graph_runner()
+            self.patched_model.get_model().cpu()
+        if 'kv_cache' in tags:
+            self.cache_engine = None
+        torch.cuda.empty_cache()
+
+    @torch.inference_mode()
+    def wakeup(self, tags: Optional[List[str]] = None):
+        """Wakeup."""
+        if tags is None:
+            tags = ['weights', 'kv_cache']
+        if 'weights' in tags:
+            self.patched_model.get_model().to(torch.cuda.current_device())
+        if 'kv_cache' in tags:
+            self.build_cache_engine()
 
     def release(self):
         """release."""
